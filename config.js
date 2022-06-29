f51690c58aa8cbed1c74cd5e74810416c98a9f6b
Than <thancoder2019@gmail.com>
2022-06-29 09:43:04 +0630

    update

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 
 const config = {
-    // mongoUri:'mongodb+srv://than:than0333@cluster0.59qwp.mongodb.net/personal',
-    mongoUri:'mongodb://localhost:27017/',
+    mongoUri:'mongodb+srv://than:than0333@cluster0.59qwp.mongodb.net/personal',
+    // mongoUri:'mongodb://localhost:27017/',
     mongodbName:'personal',
     staticPath:path.join(__dirname,'public'),
     clientPath:path.resolve(__dirname,'public'),
